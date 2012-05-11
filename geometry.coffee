e90e1118257da017c7d96ff5830229a5001d6d73
Kai Chang <kai.salmon.chang@gmail.com>
2012-05-10 23:30:07 -0700

    removed special characters

--- geometry.coffee
+++ geometry.coffee
@@ -5,25 +5,26 @@ acos = Math.acos
 atan2 = Math.atan2
 
 # Spherical to Cartesian Coordinates
-sph2car = (r,ɵ,φ) ->
-  x: r * cos(φ) * sin(ɵ)
-  y: r * sin(φ) * sin(ɵ)
-  z: r * cos(ɵ)
+sph2car = (r,theta,phi) ->
+  x: r * cos(phi) * sin(theta)
+  y: r * sin(phi) * sin(theta)
+  z: r * cos(theta)
 
 # Cartesian to Spherical Coordinates
 car2sph = (x,y,z) ->
-  r: sqrt(x*x + y*y + z*z)
-  φ: atan2(y,x)
-  ɵ: acos(z/r)
+  r = sqrt(x*x + y*y + z*z)
+  r: r
+  phi: atan2(y,x)
+  theta: acos(z/r)
 
 # Cylindrical to Spherical Coordinates
-cyl2sph = (ρ,φ,z) ->
-  r: sqrt(ρ*ρ + z*z)
-  ɵ: atan2(ρ,z)
-  φ: φ
+cyl2sph = (rho,phi,z) ->
+  r: sqrt(rho*rho + z*z)
+  theta: atan2(rho,z)
+  phi: phi
 
 # Spherical to Cylindrical Coordinates
-sph2cyl = (r,θ,φ) ->
-  ρ: r * sin(θ)
-  φ: φ
-  z: r * cos(θ)
+sph2cyl = (r,theta,phi) ->
+  rho: r * sin(theta)
+  phi: phi
+  z: r * cos(theta)
--- geometry.js
+++ geometry.js
@@ -1,46 +1,45 @@
-(function() {
-  var acos, atan2, car2sph, cos, cyl2sph, sin, sph2car, sph2cyl, sqrt;
+var acos, atan2, car2sph, cos, cyl2sph, sin, sph2car, sph2cyl, sqrt;
 
-  cos = Math.cos;
+cos = Math.cos;
 
-  sin = Math.sin;
+sin = Math.sin;
 
-  sqrt = Math.sqrt;
+sqrt = Math.sqrt;
 
-  acos = Math.acos;
+acos = Math.acos;
 
-  atan2 = Math.atan2;
+atan2 = Math.atan2;
 
-  sph2car = function(r, ɵ, φ) {
-    return {
-      x: r * cos(φ) * sin(ɵ),
-      y: r * sin(φ) * sin(ɵ),
-      z: r * cos(ɵ)
-    };
+sph2car = function(r, theta, phi) {
+  return {
+    x: r * cos(phi) * sin(theta),
+    y: r * sin(phi) * sin(theta),
+    z: r * cos(theta)
   };
+};
 
-  car2sph = function(x, y, z) {
-    return {
-      r: sqrt(x * x + y * y + z * z),
-      φ: atan2(y, x),
-      ɵ: acos(z / r)
-    };
+car2sph = function(x, y, z) {
+  var r;
+  r = sqrt(x * x + y * y + z * z);
+  return {
+    r: r,
+    phi: atan2(y, x),
+    theta: acos(z / r)
   };
+};
 
-  cyl2sph = function(ρ, φ, z) {
-    return {
-      r: sqrt(ρ * ρ + z * z),
-      ɵ: atan2(ρ, z),
-      φ: φ
-    };
+cyl2sph = function(rho, phi, z) {
+  return {
+    r: sqrt(rho * rho + z * z),
+    theta: atan2(rho, z),
+    phi: phi
   };
+};
 
-  sph2cyl = function(r, θ, φ) {
-    return {
-      ρ: r * sin(θ),
-      φ: φ,
-      z: r * cos(θ)
-    };
+sph2cyl = function(r, theta, phi) {
+  return {
+    rho: r * sin(theta),
+    phi: phi,
+    z: r * cos(theta)
   };
-
-}).call(this);
+};
